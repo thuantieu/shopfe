9d0f1923222b0aea39728c01f39716105a32994b
Thuan Tieu <tieuvinhthuan.fi@gmail.com>
2022-12-28 04:43:58 +0200

    Editing cart and creating cart summary

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,11 @@
 <template>
   <v-app>
     <v-app-bar color="primary" density="compact">
-      <!-- <template v-slot:prepend>
-          <v-app-bar-nav-icon></v-app-bar-nav-icon>
-        </template> -->
+      
 
-      <v-app-bar-title>Ball shop</v-app-bar-title>
-
-      <!-- <template v-slot:append>
-          <v-btn icon="mdi-dots-vertical"></v-btn>
-        </template> -->
+      <v-app-bar-title @click="() => $router.push({ path: '/' })"
+        ><v-btn variant="text" size="xlarge">Ball shop</v-btn>
+      </v-app-bar-title>
     </v-app-bar>
     <v-main>
       <router-view />
@@ -18,15 +14,15 @@
 </template>
 
 <script>
-import productList from '@/sampledata/products.json'
+import productList from "@/sampledata/products.json";
 export default {
   name: "App",
 
   data: () => ({
     //
   }),
-  created () {
-    this.$store.dispatch('addproductList', productList)
-  }
+  created() {
+    this.$store.dispatch("addproductList", productList);
+  },
 };
 </script>
